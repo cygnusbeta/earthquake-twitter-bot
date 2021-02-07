4da9ad16bea196087fc8e6e11d69071ad674de7c
cygnusbeta <betacygnib+git@gmail.com>
2021-02-08 08:33:24 +0900

    Config file into String

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+
+fn read_config() {
+    let config = fs::read_to_string("config/config.yml")
+        .expect("Something went wrong reading the file");
+    println!("{}", &config);
+}
+
 fn main() {
+    read_config();
     println!("Hello, world!");
 }
