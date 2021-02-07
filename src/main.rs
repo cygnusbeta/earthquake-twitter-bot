a9bd4e89f5745e99227c31d3bd1c7a59ed651902
cygnusbeta <betacygnib+git@gmail.com>
2021-02-08 08:43:22 +0900

    Add fn tweet() (draft)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use std::fs;
 use std::collections::HashMap;
 
-fn read_config() {
+fn read_config() -> HashMap<String, String> {
     let s = fs::read_to_string("config/config.yml")
         .expect("Something went wrong reading the file");
     println!("{}", &s);
     let map: HashMap<String, String> = serde_yaml::from_str(&s).unwrap();
     println!("{:?}", &map);
+    map
+}
+
+fn tweet(envs: &HashMap<String, String>) {
+    
 }
 
 fn main() {
-    read_config();
-    println!();
+    let envs = read_config();
+    tweet(&envs);
 }
