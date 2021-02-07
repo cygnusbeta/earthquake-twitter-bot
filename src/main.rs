c257fcdf561620bb8f4f28af25b771792432152a
cygnusbeta <betacygnib+git@gmail.com>
2021-02-08 08:39:22 +0900

    Config written in yaml into HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn read_config() {
-    let config = fs::read_to_string("config/config.yml")
+    let s = fs::read_to_string("config/config.yml")
         .expect("Something went wrong reading the file");
-    println!("{}", &config);
+    println!("{}", &s);
+    let map: HashMap<String, String> = serde_yaml::from_str(&s).unwrap();
+    println!("{:?}", &map);
 }
 
 fn main() {
     read_config();
-    println!("Hello, world!");
+    println!();
 }
