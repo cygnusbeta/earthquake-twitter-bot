a0bb453b7d3870df050a989946eb2adc22cd1e5d
cygnusbeta <betacygnib+git@gmail.com>
2021-03-16 01:10:45 +0900

    Add struct Scraper

--- src/scraping.rs
+++ src/scraping.rs
@@ -3,26 +3,38 @@ use scraper::{Html, Selector};
 
 #[path = "util.rs"] mod util;
 
-fn scrape(url: String, selector: String) -> Result<String, Box<dyn std::error::Error>> {
-    println!("scraping... {}", &url);
-    let resp = reqwest::blocking::get(url)?;
-    println!("-> response: `{}`", &resp.status());
-    assert!(resp.status().is_success());
-    let body = resp.text()?;
-    let document = Html::parse_document(&body);
-    let selector = Selector::parse(selector.as_str()).unwrap();
-    let elements = document.select(&selector);
-    let text = elements.map(|e| format!("{}", e.text().collect::<Vec<_>>().join(" "))).collect::<Vec<_>>().join(" ");
-    assert!(!&text.is_empty(), "selector not found");
-    println!("   scraped text: `{}`", &text);
-    Ok(text)
+struct Scraper {
+    body: String
 }
 
+impl Scraper {
+    fn fetch(url: String) -> Result<Self, Box<dyn std::error::Error>> {
+        println!("scraping... {}", &url);
+        let resp = reqwest::blocking::get(&url)?;
+        println!("-> response: `{}`", &resp.status());
+        assert!(resp.status().is_success());
+        let body = resp.text()?;
+        Ok(Self {
+            body: body
+        })
+    }
+
+    fn select(&self, selector: String) -> Result<String, Box<dyn std::error::Error>> {
+        let document = Html::parse_document(&self.body);
+        let selector = Selector::parse(selector.as_str()).unwrap();
+        let elements = document.select(&selector);
+        let text = elements.map(|e| format!("{}", e.text().collect::<Vec<_>>().join(" "))).collect::<Vec<_>>().join(" ");
+        assert!(!&text.is_empty(), "selector not found");
+        println!("   scraped text: `{}`", &text);
+        Ok(text)
+    }
+}
+
+
+
 fn get_ri() {
-    scrape(
-        "http://157.80.67.225/".to_string(),
-        "body > table > tbody > tr > td > div:nth-child(3) > ul > li:nth-child(2) > strong:nth-child(1)".to_string()
-    ).unwrap();
+    let scraper = Scraper::fetch("http://157.80.67.225/".to_string()).unwrap();
+    let text = scraper.select("body > table > tbody > tr > td > div:nth-child(3) > ul > li:nth-child(2) > strong:nth-child(1)".to_string()).unwrap();
 }
 
 fn main() {
