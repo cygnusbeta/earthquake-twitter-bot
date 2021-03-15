dbfdafdfaffe309df4d2036db78eec2cce5fff19
cygnusbeta <betacygnib+git@gmail.com>
2021-03-16 00:55:17 +0900

    [scraping] Add scraping code and test url

--- src/scraping.rs
+++ src/scraping.rs
@@ -1,20 +1,28 @@
 use reqwest;
-use tokio::runtime::Runtime;
-use std::future::Future;
-use std::error::Error;
-use reqwest::Response;
-use crate::util::rt;
+use scraper::{Html, Selector};
 
 #[path = "util.rs"] mod util;
 
-fn get_ri() {
-    let url = "https://www.google.com/".to_string();
-    let resp = rt().block_on(async {
-        let mut resp = reqwest::get(url).await.unwrap();
-        resp
-    });
-    println!("{}", &resp.status());
+fn scrape(url: String, selector: String) -> Result<String, Box<dyn std::error::Error>> {
+    println!("scraping... {}", &url);
+    let resp = reqwest::blocking::get(url)?;
+    println!("-> response: `{}`", &resp.status());
     assert!(resp.status().is_success());
+    let body = resp.text()?;
+    let document = Html::parse_document(&body);
+    let selector = Selector::parse(selector.as_str()).unwrap();
+    let elements = document.select(&selector);
+    let text = elements.map(|e| format!("{}", e.text().collect::<Vec<_>>().join(" "))).collect::<Vec<_>>().join(" ");
+    assert!(!&text.is_empty(), "selector not found");
+    println!("   scraped text: `{}`", &text);
+    Ok(text)
+}
+
+fn get_ri() {
+    scrape(
+        "http://evrrss.eri.u-tokyo.ac.jp/db/index.html".to_string(),
+        "body > font > blockquote > h1".to_string()
+    ).unwrap();
 }
 
 fn main() {
