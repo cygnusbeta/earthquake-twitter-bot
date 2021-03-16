f869d89ba2c30f406239284802080f2dc7809a81
cygnusbeta <betacygnib+git@gmail.com>
2021-03-16 13:42:05 +0900

    [cargo, util] Use tokio 0.2.25

--- src/util.rs
+++ src/util.rs
@@ -3,10 +3,7 @@ use tokio::runtime::Runtime;
 
 #[allow(dead_code)]
 pub fn rt() -> Runtime {
-    let rt = tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .unwrap();
+    let rt = Runtime::new().unwrap();
     rt
 }
 
