d30a140c5cfc8ce3c433004146ae952dcaa03789
cygnusbeta <betacygnib+git@gmail.com>
2023-05-31 19:11:46 +0900

    Remove egg-mode

--- src/tweeting.rs
+++ src/tweeting.rs
@@ -1,14 +1,11 @@
 use std::fs;
 use std::collections::HashMap;
-use egg_mode::tweet::DraftTweet;
-use egg_mode::Token;
-use egg_mode::media::{upload_media, media_types};
 use std::fs::File;
 use std::io::Read;
 use util::{rt, Result};
 use twitter_v2::TwitterApi;
 use twitter_v2::authorization::Oauth1aToken;
-// use Oauth1aToken as Token;
+use Oauth1aToken as Token;
 
 #[path = "util.rs"] mod util;
 
@@ -21,7 +18,7 @@ fn read_config(config_path: String) -> HashMap<String, String> {
     map
 }
 
-pub fn create_token(config_path: String) -> Oauth1aToken {
+pub fn create_token(config_path: String) -> Token {
     let envs = read_config(config_path);
 
     let consumer_key = envs["consumer_key"].clone();
@@ -34,7 +31,7 @@ pub fn create_token(config_path: String) -> Oauth1aToken {
 }
 
 #[allow(dead_code)]
-pub async fn tweet(body: String, token: &Oauth1aToken) -> Result<()> {
+pub async fn tweet(body: String, token: &Token) -> Result<()> {
     println!("Tweeting...");
 
     let response = TwitterApi::new(token.clone())
@@ -56,19 +53,19 @@ pub async fn tweet(body: String, token: &Oauth1aToken) -> Result<()> {
     Ok(())
 }
 
-// #[allow(dead_code)]
-// pub fn tweet_await(body: String, token: &Token) -> Result<()> {
-//     let res = rt().block_on(async {
-//         // tweet("test2".to_string(), &token).await.unwrap();
-//         let res = tweet(body, &token).await;
-//         let res = match res {
-//             Ok(_) => Ok(()),
-//             Err(e) => Err(e)
-//         };
-//         res
-//     });
-//     res
-// }
+#[allow(dead_code)]
+pub fn tweet_await(body: String, token: &Token) -> Result<()> {
+    let res = rt().block_on(async {
+        // tweet("test2".to_string(), &token).await.unwrap();
+        let res = tweet(body, &token).await;
+        let res = match res {
+            Ok(_) => Ok(()),
+            Err(e) => Err(e)
+        };
+        res
+    });
+    res
+}
 
 #[allow(dead_code)]
 fn read_img(img_path: &String) -> Vec<u8> {
@@ -80,42 +77,42 @@ fn read_img(img_path: &String) -> Vec<u8> {
     image
 }
 
-#[allow(dead_code)]
-pub async fn tweet_w_img(body: String, img_path: String, token: &Token) -> Result<()> {
-    let image_fname = img_path.clone().split('/').collect::<Vec<_>>().last().unwrap().to_string();
-    println!("Uploading image: `{}`...", &image_fname);
-    let handle = upload_media(&read_img(&img_path), &media_types::image_png(), &token).await?;
-    println!("Successfully uploaded.");
+// #[allow(dead_code)]
+// pub async fn tweet_w_img(body: String, img_path: String, token: &Token) -> Result<()> {
+//     let image_fname = img_path.clone().split('/').collect::<Vec<_>>().last().unwrap().to_string();
+//     println!("Uploading image: `{}`...", &image_fname);
+//     let handle = upload_media(&read_img(&img_path), &media_types::image_png(), &token).await?;
+//     println!("Successfully uploaded.");
+//
+//     println!("Tweeting...");
+//     let mut draft = DraftTweet::new(body);
+//     draft.add_media(handle.id);
+//     let post = draft.send(&token).await?;
+//     let user = post.response.user.unwrap();
+//     println!("Successfully tweeted:");
+//     println!("@{} `{}`: `{}` (image: `{}`)", &user.screen_name, &user.name, &post.response.text, &image_fname);
+//     Ok(())
+// }
 
-    println!("Tweeting...");
-    let mut draft = DraftTweet::new(body);
-    draft.add_media(handle.id);
-    let post = draft.send(&token).await?;
-    let user = post.response.user.unwrap();
-    println!("Successfully tweeted:");
-    println!("@{} `{}`: `{}` (image: `{}`)", &user.screen_name, &user.name, &post.response.text, &image_fname);
-    Ok(())
-}
-
-#[allow(dead_code)]
-pub fn tweet_w_img_await(body: String, img_path: String, token: &Token) -> Result<()> {
-    let res = rt().block_on(async {
-        // tweet("test2".to_string(), &token).await.unwrap();
-        let res = tweet_w_img(body, img_path, &token).await;
-        let res = match res {
-            Ok(_) => Ok(()),
-            Err(e) => Err(e)
-        };
-        res
-    });
-    res
-}
+// #[allow(dead_code)]
+// pub fn tweet_w_img_await(body: String, img_path: String, token: &Token) -> Result<()> {
+//     let res = rt().block_on(async {
+//         // tweet("test2".to_string(), &token).await.unwrap();
+//         let res = tweet_w_img(body, img_path, &token).await;
+//         let res = match res {
+//             Ok(_) => Ok(()),
+//             Err(e) => Err(e)
+//         };
+//         res
+//     });
+//     res
+// }
 
 #[allow(dead_code)]
 fn main() {
     let token = create_token("config/config.yml".to_string());
     rt().block_on(async {
         tweet("テストツイート（bot のメンテナンス中です）".to_string(), &token).await.unwrap();
-        // tweet_w_img("test w/img".to_string(), "assets/test/test.png".to_string(), &token).await.unwrap();
+        // tweet_w_img("テストツイート（bot のメンテナンス中です）".to_string(), "assets/test/test.png".to_string(), &token).await.unwrap();
     });
 }
